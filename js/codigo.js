0259f7c4eb7fc1cf78bd5fea2e8b50e239fda1e3
Brayan <146513370+BrayanSLopez@users.noreply.github.com>
2024-11-03 17:37:10 -0500

    Add files via upload

---
+++ js/codigo.js
@@ -0,0 +1,31 @@
+function redirectInicio() {
+    window.location.href = "./index.html";
+}
+
+function redirectSemestreI_II() {
+    window.location.href = "./SemestreI_II.html";
+}
+
+function redirectSemestreIII() {
+    window.location.href = "./SemestreIII.html";
+}
+/*codigo carrusel*/
+let currentIndex = 0;
+
+function moveSlide(direction) {
+    const slides = document.querySelectorAll('.carrusel-item');
+    const totalSlides = slides.length;
+    
+    currentIndex += direction;
+    
+    if (currentIndex >= totalSlides) {
+        currentIndex = 0;
+    }
+    if (currentIndex < 0) {
+        currentIndex = totalSlides - 1;
+    }
+    
+    const carouselSlide = document.querySelector('.carrusel-slide');
+    carouselSlide.style.transform = `translateX(-${currentIndex * 100}%)`;
+}
+/*codigo carrusel*/
